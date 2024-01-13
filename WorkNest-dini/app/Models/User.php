5abd9adeaf05e6e28fc1880f830e2c2ea6df7643
Dini Damayanti <dini.gamelab@gmail.com>
2024-01-13 12:36:35 +0700

    WorkNest-dini

---
+++ WorkNest-dini/app/Models/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role', // tambahkan role ke fillable
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function isAdmin()
+    {
+        return $this->role === 'admin'; // asumsi 'role' adalah field di tabel users
+    }
+
+    public function pekerja()
+    {
+        return $this->hasMany(Pekerja::class);
+    }
+}
